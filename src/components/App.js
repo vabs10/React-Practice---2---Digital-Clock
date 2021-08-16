094a1bc7c93acd5c3f9271ddecd53b8c6bdf83b8
vabs10 <vyagneshwar98@gmail.com>
2021-08-16 20:04:27 +0530

    Made the Clock

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,37 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+function getCurrentTime() {
+    const currentDate = new Date();
+    return currentDate.toLocaleTimeString().toUpperCase();
 }
 
+class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: getCurrentTime(),
+        };
+    }
+    componentDidMount() {
+        this.xyz = setInterval(() => {
+            this.setState({ time: getCurrentTime() });
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.xyz);
+    }
+
+    render() {
+        return(
+            <div className="Clock">
+                <h3 id="time">
+                {this.state.time}
+                </h3>
+            </div>
+        );
+    }
+}
 
 export default App;
